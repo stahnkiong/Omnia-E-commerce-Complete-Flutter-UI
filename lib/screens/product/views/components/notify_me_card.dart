b802837b1f4051691e964d022b3cdc3a7e2ee7fc
stahnkiong <stahn85@gmail.com>
2025-12-01 11:37:45 +0800

    agp kgp gradle update

--- lib/screens/product/views/components/notify_me_card.dart
+++ lib/screens/product/views/components/notify_me_card.dart
@@ -70,7 +70,7 @@ class NotifyMeCard extends StatelessWidget {
                 CupertinoSwitch(
                   onChanged: onChanged,
                   value: isNotify,
-                  activeColor: primaryMaterialColor.shade900,
+                  activeTrackColor: primaryMaterialColor.shade900,
                 )
               ],
             ),
